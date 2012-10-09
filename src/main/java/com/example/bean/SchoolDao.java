c6c71f0f47e1cb30ed7f3d4673a8a3fb9f3dfe17
hceylan <hceylan@ceylan.batoo.org>
2012-10-09 13:28:33 +0300

    Initial Commit

---
+++ src/main/java/com/example/bean/SchoolDao.java
@@ -0,0 +1,71 @@
+package com.example.bean;
+
+import java.util.List;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+import com.example.model.BaseEntity;
+import com.example.model.Course;
+import com.example.model.Person;
+import com.example.model.Student;
+import com.example.model.Teacher;
+
+/**
+ * This is the interface for the SchoolDao implementations. In our case, it
+ * doubles as the {@link Local} EJB interface for our {@link Stateless} bean.
+ * Currently, this is only implemented in {@link SchoolJpaDao}.
+ * 
+ * @author Andy Gibson
+ * 
+ */
+@Local
+public interface SchoolDao {
+
+	public Person getPerson(Long id);
+
+	public Teacher getTeacher(Long id);
+
+	public Student getStudent(Long id);
+
+	public Course getCourse(Long id);
+
+	public List<Teacher> getTeachers();
+
+	public List<Course> getCourses();
+
+	public List<Student> getStudentsForCourse(Course course);
+
+	public List<Course> getCoursesForStudent(Student student);
+
+	public List<Course> getCoursesForTeacher(Teacher teacher);
+
+	/**
+	 * Returns a list of {@link Student} objects based on the search and
+	 * pagination criteria.
+	 * 
+	 * @param criteria
+	 *            The criteria for which we get the results
+	 * @param firstResult
+	 *            Index of the first result to return
+	 * @param pageSize
+	 *            The number of results to return
+	 * @return List of {@link Student} entities matching the criteria
+	 */
+	public List<Student> searchStudents(SearchCriteria criteria,
+			int firstResult, int pageSize);
+
+	public <T extends BaseEntity> T save(T entity);
+
+	public <T extends BaseEntity> void refresh(T entity);
+
+	public void notifyCourseChanges(Student student,
+			List<Course> originalCourseList);
+
+	/**
+	 * Returns a list of teachers with the number of courses they teach.
+	 * 
+	 * @return List of {@link TeacherCourseCount} objects
+	 */
+	public List<TeacherCourseCount> getTeachCourseCounts();
+}
---
+++ src/main/java/com/example/event/StudentCoursePayload.java
@@ -0,0 +1,45 @@
+package com.example.event;
+
+import com.example.model.Course;
+import com.example.model.Student;
+
+/**
+ * This class is used to define the payload for a student/course based event.
+ * This allows us to pass both a student and a course at the same time as one
+ * object.
+ * 
+ * @author Andy Gibson
+ * 
+ */
+public class StudentCoursePayload {
+
+	private Student student;
+	private Course course;
+
+	public StudentCoursePayload() {
+
+	}
+
+	public StudentCoursePayload(Student student, Course course) {
+		super();
+		this.student = student;
+		this.course = course;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public Course getCourse() {
+		return course;
+	}
+
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+
+}
